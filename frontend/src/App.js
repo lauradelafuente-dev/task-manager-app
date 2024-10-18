cfe3306bd1700352a7156d8cc36c82277be8d229
Eduan Moraes <lauradelafuente.dev@gmail.com>
2024-10-18 19:10:39 +0900

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';  // Asegúrate de usar la ruta correcta
+import Kanban from './components/Kanban';  // Ruta corregida
+
+function App() {
+  return (
+    <div className="App">
+      <Kanban />
+    </div>
+  );
+}
+
+export default App;
